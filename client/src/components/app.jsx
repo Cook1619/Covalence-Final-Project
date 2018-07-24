fdbb3e2fb1003169ef9686dedb9c40336b5caea6
Cook1619 <cook6348@gmail.com>
2018-07-24 10:29:12 -0500

    deleted hello and goodbye components

--- client/src/components/app.jsx
+++ client/src/components/app.jsx
@@ -1,8 +1,5 @@
 import React, { Component, Fragment } from 'react';
 import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
-
-import HelloWorld from './hello';
-import GoodbyeWorld from './goodbye';
 import Donate from './donate';
 import DisplayGames from './DisplayGames';
 import PrivateRoute from './auth/privateRoute';
@@ -17,17 +14,14 @@ class Navigation extends Component {
             <Router>
                 <Fragment>
                     <nav className="navbar navbar-expand-lg navbar-light bg-dark">
-                        <Link className="nav-item nav-link" to="/goodbye">Goodbye</Link>
-                        <Link className="nav-item nav-link" to="/games">Games</Link>
+                        <Link className="nav-item nav-link" to="/">Games</Link>
                         <AuthButton />
                     </nav>
                     <Switch>
-                        <Route exact path="/" component={HelloWorld} />
                         <Route path="/login" component={Login} />
-                        <Route path="/games" component={DisplayGames} />
+                        <Route path="/" component={DisplayGames} />
                         <Route path="/logout" component={Logout} />
                         <Route path="/donate" component={Donate} />
-                        <Route path="/goodbye" component={GoodbyeWorld} />
                     </Switch>
                 </Fragment>
             </Router>
