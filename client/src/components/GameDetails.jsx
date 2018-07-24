585f2e42ca48996df9ac3403f3f2efd47454ec64
percussion1313 <peterjacobgrant@gmail.com>
2018-07-24 17:50:58 -0500

    Added new homepage image. Removed jumbotron

--- client/src/components/GameDetails.jsx
+++ client/src/components/GameDetails.jsx
@@ -8,11 +8,9 @@ class GameDetails extends Component {
 
     render() {
         return (
-            <div className="container">
-                <div className="row">
-                    <div className="col-6">
+                    <div className="col-6 position-static">
                         <div className="card p-2 m-4 bg-light rounded-3">
-                            <div className="card-header bg-dark text-white rounded-3">Some text here</div>
+                            <div className="card-header bg-dark text-white rounded-0">Some text here</div>
                             <div className="card-body shadow">
                                 <h3 className="card-title">Matchup Preview</h3>
                                 <p className="card-text">{`${this.props.game.team1} VS ${this.props.game.team2}`}</p>
@@ -20,8 +18,6 @@ class GameDetails extends Component {
                             </div>
                         </div>
                     </div>
-                </div>
-            </div>
         )
     }
 }
--- client/src/components/HomePage.jsx
+++ client/src/components/HomePage.jsx
@@ -1,16 +1,10 @@
 import React from 'react';
-import background from './img/baseball.png'
+import background from './img/homepage.png'
 
 const HomePage = () => {
     return (
         <div>
-            <div className="container">
-                <div className="jumbotron m-3">
-                    <h1>Welcome to the Strike Zone</h1>
-                    <p>The place to come for all your baseball betting needs</p>
-                </div>
-            </div>
-            <img className="m-5 img-responsive" src={background} alt="baseball" />
+            <img className="img-responsive w-100" src={background} alt="baseball" />
         </div>
     )
 }
