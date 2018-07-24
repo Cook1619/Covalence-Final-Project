19747bfd43ab1c2c4fdcdf9bfea424efa778b9ef
Cook1619 <cook6348@gmail.com>
2018-07-24 16:37:32 -0500

    Settting up Contact page, brought in a font, and playing with Homepage

--- client/src/components/HomePage.jsx
+++ client/src/components/HomePage.jsx
@@ -2,9 +2,15 @@ import React from 'react';
 import background from './img/baseball.png'
 
 const HomePage = () => {
-    return(
+    return (
         <div>
-            <img className="m-5" src={background} alt="baseball" />
+            <div className="container">
+                <div className="jumbotron m-3">
+                    <h1>Welcome to the Strike Zone</h1>
+                    <p>The place to come for all your baseball betting needs</p>
+                </div>
+            </div>
+            <img className="m-5 img-responsive" src={background} alt="baseball" />
         </div>
     )
 }
--- client/src/components/app.jsx
+++ client/src/components/app.jsx
@@ -7,6 +7,7 @@ import Login from './auth/login';
 import Logout from './auth/logout';
 import AuthButton from './auth/authButton';
 import HomePage from './HomePage';
+import Contact from './Contact';
 
 class Navigation extends Component {
 
@@ -15,12 +16,15 @@ class Navigation extends Component {
             <Router>
                 <Fragment>
                     <nav className="navbar navbar-light bg-dark">
+                        <Link className="btn btn-outline-info btn-sml" to="/">Home</Link>
                         <Link className="btn btn-outline-info btn-sml" to="/games">Games</Link>
+                        <Link className="btn btn-outline-info btn-sml" to="/contact">Contact</Link>
                         <AuthButton className="nav-item nav-link"/>
                     </nav>
                     <Switch>
                         <Route exact path="/" component={HomePage} />
                         <Route path="/login" component={Login} />
+                        <Route path="/contact" component={Contact} />
                         <Route path="/games" component={DisplayGames} />
                         <Route path="/logout" component={Logout} />
                         <Route path="/donate" component={Donate} />
