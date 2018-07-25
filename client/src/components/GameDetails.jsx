8daf78643e691e24715d96137ca9ad4ffd5dfe03
Cook1619 <cook6348@gmail.com>
2018-07-25 08:05:26 -0500

    Cards still not positioning how I want them too, I want them to go side by side not stack

--- client/src/components/GameDetails.jsx
+++ client/src/components/GameDetails.jsx
@@ -9,17 +9,31 @@ class GameDetails extends Component {
 
     render() {
         return (
-            <div className="col-6 position-static">
-                <div className="card p-2 m-4 bg-light rounded-3">
-                    <div className="card-header bg-dark text-white rounded-0">Some text here</div>
-                    <div className="card-body shadow">
-                        <h3 className="card-title">Matchup Preview</h3>
-                        <p className="card-text">{`${this.props.game.team1} VS ${this.props.game.team2}`}</p>
-                        <button className="btn btn-sm btn-success float-right">Bet Now</button>
+            <div>
+                <div className="row">
+                    <div className="col-sm-6">
+                        <div className="card p-2 m-4 bg-light rounded-3">
+                            <div className="card-body">
+                                <h5 className="card-header bg-dark text-white rounded-0">Some Text here</h5>  
+                                <div className="card-title"> Matchup Preview</div>
+                                <p className="card-text">{`${this.props.game.team1} VS ${this.props.game.team2}`}</p>
+                                <button className="btn btn-sm btn-success float-right">Bet Now</button>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
 
+            // <div className="col-6 position-static">
+            //     <div className="card p-2 m-4 bg-light rounded-3">
+            //         <div className="card-header bg-dark text-white rounded-0">Some text here</div>
+            //         <div className="card-body shadow">
+            //             <h3 className="card-title">Matchup Preview</h3>
+            //             <p className="card-text">{`${this.props.game.team1} VS ${this.props.game.team2}`}</p>
+            //             <button className="btn btn-sm btn-success float-right">Bet Now</button>
+            //         </div>
+            //     </div>
+            // </div>
         )
     }
 }
