8a8be9361eff5cf5a2a56cd69c6951540592460e
Cook1619 <cook6348@gmail.com>
2018-07-25 20:13:41 -0500

    Fixed errors

--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -18,37 +18,37 @@ class Register extends Component {
                 <div className="container">
                     <form>
                         <div className="form-group row">
-                            <label for="FirstName" className="col-sm-2 col-form-label">First Name</label>
+                            <label htmlFor="FirstName" className="col-sm-2 col-form-label">First Name</label>
                             <div className="col-sm-10">
                                 <input type="FirstName" className="form-control" id="Firstname" placeholder="First Name" />
                             </div>
                         </div>
                         <div className="form-group row">
-                            <label for="LastName" className="col-sm-2 col-form-label">Last Name</label>
+                            <label htmlFor="LastName" className="col-sm-2 col-form-label">Last Name</label>
                             <div className="col-sm-10">
                                 <input type="LastName" className="form-control" id="LastName" placeholder="Last Name" />
                             </div>
                         </div>
                         <div className="form-group row">
-                            <label for="EMAIL" className="col-sm-2 col-form-label">Email</label>
+                            <label htmlFor="EMAIL" className="col-sm-2 col-form-label">Email</label>
                             <div className="col-sm-10">
                                 <input type="email" className="form-control" id="EMAIL" placeholder="Email" />
                             </div>
                         </div>
                         <div className="form-group row">
-                            <label for="PASSWORD" className="col-sm-2 col-form-label">Password</label>
+                            <label htmlFor="PASSWORD" className="col-sm-2 col-form-label">Password</label>
                             <div className="col-sm-10">
                                 <input type="password" className="form-control" id="PASSWORD" placeholder="Password"/>
                             </div>
                         </div>
                         <div className="form-group row">
-                            <label for="CONFIRMPASSWORD" className="col-sm-2 col-form-label">Confirm Password</label>
+                            <label htmlFor="CONFIRMPASSWORD" className="col-sm-2 col-form-label">Confirm Password</label>
                             <div className="col-sm-10">
                                 <input type="password" className="form-control" id="CONFIRMPASSWORD" placeholder="Confirm Password" />
                             </div>
                         </div>
                         <div className="form-group row">
-                            <label for="USERNAME" className="col-sm-2 col-form-label">User Name</label>
+                            <label htmlFor="USERNAME" className="col-sm-2 col-form-label">User Name</label>
                             <div className="col-sm-10">
                                 <input type="User Name" className="form-control" id="USERNAME" placeholder="User Name" />
                             </div>
