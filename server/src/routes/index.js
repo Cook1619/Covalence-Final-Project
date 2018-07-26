8317f147805ab0d0e9661f2f0d8f40f310fc5920
Billy Hughes <billyhughes@Users-MacBook-Pro.local>
2018-07-26 09:35:34 -0500

    added ENV

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -9,13 +9,13 @@ import { isLoggedIn, tokenMiddleware } from '../middleware/auth.mw';
 let router = Router();
 
 router.use('/auth', authRouter);
-router.use('/donate', stripeDonationsRouter);
+
 
 router.route('*')
     .post(tokenMiddleware, isLoggedIn)
     .put(tokenMiddleware, isLoggedIn)
     .delete(tokenMiddleware, isLoggedIn);
-
+router.use('/donate', stripeDonationsRouter);
 router.use('/classes', classesRouter);
 router.use('/people', peopleRouter);
 router.use('/users', usersRouter);
