f6c405baf5c8604c66bced5e0faed66a006d137d
Cook1619 <cook6348@gmail.com>
2018-07-26 06:47:44 -0500

    settings up onChange events for state in the form

--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -12,6 +12,32 @@ class Register extends Component {
         }
 
     }
+    handleFirstName(event){
+        this.setState({
+            firstName: event.target.value
+        })
+    }
+    handleLastName(event) {
+        this.setState({
+            lastName: event.target.value
+        })
+    }
+    handleEmail(event){
+        this.setState({
+            email: event.target.value
+        })
+    }
+    handleEmail(event){
+        this.setState({
+            password: event.target.value
+        })
+    }
+    handleUserName(event){
+        this.setState({
+            username: event.target.value
+        })
+    }
+
     render() {
         return (
             <Fragment>
@@ -20,25 +46,49 @@ class Register extends Component {
                         <div className="form-group row">
                             <label htmlFor="FirstName" className="col-sm-2 col-form-label">First Name</label>
                             <div className="col-sm-10">
-                                <input type="FirstName" className="form-control" id="Firstname" placeholder="First Name" />
+                                <input 
+                                type="FirstName" 
+                                className="form-control" 
+                                id="Firstname" 
+                                placeholder="First Name"
+                                onChange={this.handleFirstName.bind}
+                                />
                             </div>
                         </div>
                         <div className="form-group row">
                             <label htmlFor="LastName" className="col-sm-2 col-form-label">Last Name</label>
                             <div className="col-sm-10">
-                                <input type="LastName" className="form-control" id="LastName" placeholder="Last Name" />
+                                <input 
+                                type="LastName" 
+                                className="form-control" 
+                                id="LastName" 
+                                placeholder="Last Name"
+                                onChange={this.handleLastName.bind}
+                                />
                             </div>
                         </div>
                         <div className="form-group row">
                             <label htmlFor="EMAIL" className="col-sm-2 col-form-label">Email</label>
                             <div className="col-sm-10">
-                                <input type="email" className="form-control" id="EMAIL" placeholder="Email" />
+                                <input 
+                                type="email" 
+                                className="form-control" 
+                                id="EMAIL" 
+                                placeholder="Email"
+                                onChange={this.handleEmail.bind}
+                                />
                             </div>
                         </div>
                         <div className="form-group row">
                             <label htmlFor="PASSWORD" className="col-sm-2 col-form-label">Password</label>
                             <div className="col-sm-10">
-                                <input type="password" className="form-control" id="PASSWORD" placeholder="Password" />
+                                <input 
+                                type="password" 
+                                className="form-control" 
+                                id="PASSWORD" 
+                                placeholder="Password" 
+                                onChange={this.handlePassword.bind}
+                                />
                             </div>
                         </div>
                         <div className="form-group row">
@@ -50,7 +100,13 @@ class Register extends Component {
                         <div className="form-group row">
                             <label htmlFor="USERNAME" className="col-sm-2 col-form-label">User Name</label>
                             <div className="col-sm-10">
-                                <input type="User Name" className="form-control" id="USERNAME" placeholder="User Name" />
+                                <input 
+                                type="User Name" 
+                                className="form-control" 
+                                id="USERNAME" 
+                                placeholder="User Name" 
+                                onChange={this.handleUserName.bind}
+                                />
                             </div>
                         </div>
                         <div className="form-group row">
