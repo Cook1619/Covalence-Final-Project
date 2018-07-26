479ee91c14d1fa1f27737ef9c3ff6dd4a1c0b35d
Cook1619 <cook6348@gmail.com>
2018-07-26 06:56:47 -0500

    Set up to add users to the databse using fake endpoints for now

--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -1,4 +1,5 @@
 import React, { Component, Fragment } from 'react';
+import * as userServices from '../services/registerusers';
 
 class Register extends Component {
     constructor(props) {
@@ -12,7 +13,7 @@ class Register extends Component {
         }
 
     }
-    handleFirstName(event){
+    handleFirstName(event) {
         this.setState({
             firstName: event.target.value
         })
@@ -22,20 +23,30 @@ class Register extends Component {
             lastName: event.target.value
         })
     }
-    handleEmail(event){
+    handleEmail(event) {
         this.setState({
             email: event.target.value
         })
     }
-    handleEmail(event){
+    handleEmail(event) {
         this.setState({
             password: event.target.value
         })
     }
-    handleUserName(event){
+    handleUserName(event) {
         this.setState({
             username: event.target.value
         })
+    }
+    addUser() {
+        let userInfo = {
+            firstName: this.state.firstName,
+            lastName: this.state.lastName,
+            email: this.state.email,
+            password: this.state.password,
+            username: this.state.username,
+        }
+        userServices.insert(userInfo);
     }
 
     render() {
@@ -46,48 +57,48 @@ class Register extends Component {
                         <div className="form-group row">
                             <label htmlFor="FirstName" className="col-sm-2 col-form-label">First Name</label>
                             <div className="col-sm-10">
-                                <input 
-                                type="FirstName" 
-                                className="form-control" 
-                                id="Firstname" 
-                                placeholder="First Name"
-                                onChange={this.handleFirstName.bind}
+                                <input
+                                    type="FirstName"
+                                    className="form-control"
+                                    id="Firstname"
+                                    placeholder="First Name"
+                                    onChange={this.handleFirstName.bind}
                                 />
                             </div>
                         </div>
                         <div className="form-group row">
                             <label htmlFor="LastName" className="col-sm-2 col-form-label">Last Name</label>
                             <div className="col-sm-10">
-                                <input 
-                                type="LastName" 
-                                className="form-control" 
-                                id="LastName" 
-                                placeholder="Last Name"
-                                onChange={this.handleLastName.bind}
+                                <input
+                                    type="LastName"
+                                    className="form-control"
+                                    id="LastName"
+                                    placeholder="Last Name"
+                                    onChange={this.handleLastName.bind}
                                 />
                             </div>
                         </div>
                         <div className="form-group row">
                             <label htmlFor="EMAIL" className="col-sm-2 col-form-label">Email</label>
                             <div className="col-sm-10">
-                                <input 
-                                type="email" 
-                                className="form-control" 
-                                id="EMAIL" 
-                                placeholder="Email"
-                                onChange={this.handleEmail.bind}
+                                <input
+                                    type="email"
+                                    className="form-control"
+                                    id="EMAIL"
+                                    placeholder="Email"
+                                    onChange={this.handleEmail.bind}
                                 />
                             </div>
                         </div>
                         <div className="form-group row">
                             <label htmlFor="PASSWORD" className="col-sm-2 col-form-label">Password</label>
                             <div className="col-sm-10">
-                                <input 
-                                type="password" 
-                                className="form-control" 
-                                id="PASSWORD" 
-                                placeholder="Password" 
-                                onChange={this.handlePassword.bind}
+                                <input
+                                    type="password"
+                                    className="form-control"
+                                    id="PASSWORD"
+                                    placeholder="Password"
+                                    onChange={this.handlePassword.bind}
                                 />
                             </div>
                         </div>
@@ -100,18 +111,22 @@ class Register extends Component {
                         <div className="form-group row">
                             <label htmlFor="USERNAME" className="col-sm-2 col-form-label">User Name</label>
                             <div className="col-sm-10">
-                                <input 
-                                type="User Name" 
-                                className="form-control" 
-                                id="USERNAME" 
-                                placeholder="User Name" 
-                                onChange={this.handleUserName.bind}
+                                <input
+                                    type="User Name"
+                                    className="form-control"
+                                    id="USERNAME"
+                                    placeholder="User Name"
+                                    onChange={this.handleUserName.bind}
                                 />
                             </div>
                         </div>
                         <div className="form-group row">
                             <div className="offset-sm-2 col-sm-10">
-                                <button type="submit" className="btn btn-primary">Submit</button>
+                                <button 
+                                type="submit" 
+                                className="btn btn-primary"
+                                onChange={this.addUser.bind}
+                                >Submit</button>
                             </div>
                         </div>
                     </form>
