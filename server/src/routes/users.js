d1b81541088a7b90dd170a7743448a34533adc6d
percussion1313 <peterjacobgrant@gmail.com>
2018-07-27 13:40:16 -0500

    Added post route for users in back end

--- server/src/routes/users.js
+++ server/src/routes/users.js
@@ -1,10 +1,27 @@
 import { Router } from 'express';
 import { tokenMiddleware, isLoggedIn } from '../middleware/auth.mw';
-
+import { generateHash } from '../utils/hash';
 let router = Router();
+
+let newUser = new Table('users');
 
 router.get('/me', tokenMiddleware, isLoggedIn, (req, res) => {
     res.json(req.user);
 });
+
+router.post('/users/', async (req,res) => {
+
+    let password = generateHash(req.body.password);
+    newUser.update({
+        name: req.body.name,
+        email: req.body.email,
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        
+        password
+      }).then(user => {
+        res.json(user)
+      });
+})
 
 export default router;
