3f541d747468dd9ec4628571016cedcd6101a549
Cook1619 <cook6348@gmail.com>
2018-07-28 20:40:06 -0500

    backend sports route is wokring

--- server/src/routes/sports.js
+++ server/src/routes/sports.js
@@ -1,15 +1,17 @@
 import { Router } from 'express';
 const MySportsFeeds = require('mysportsfeeds-node');
-const msf = new MySportsFeeds('1.0', true, null);
+const msf = new MySportsFeeds('1.0', true);
 
 msf.authenticate("db265894-6d32-4d76-b616-0a127c", "Kino81672141!");
-var data = msf.getData('mlb', '2018-regular', 'scoreboard', 'json', { fordate: '20180521' });
+var data = msf.getData('mlb', '2018-regular', 'scoreboard', 'json',{fordate:'20180519'});
 
 let router = Router();
 
 router.get('/', (req, res) => {
     res.json(req.body);
     console.log(req.body)
-});
+}, (err)=>{
+    console.log(err);
+} );
 
 export default router;
