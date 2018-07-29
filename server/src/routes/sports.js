44125951c407195a0d759eac1e2356b18cf14b23
Cook1619 <cook6348@gmail.com>
2018-07-29 13:27:26 -0500

    This API is driving me nuts

--- server/src/routes/sports.js
+++ server/src/routes/sports.js
@@ -1,9 +1,12 @@
 import { Router } from 'express';
+const express =  require('express');
+let app = express();
+const path = require('path');
 const MySportsFeeds = require('mysportsfeeds-node');
 const msf = new MySportsFeeds('1.0', true);
 
 msf.authenticate("db265894-6d32-4d76-b616-0a127c", "Kino81672141!");
-var data = msf.getData('mlb', '2018-regular', 'scoreboard', 'json',{fordate:'20180519'});
+var data = msf.getData('mlb', '2018-regular', 'scoreboard', 'json',{'for-date':'20180527'});
 
 let router = Router();
 
@@ -13,5 +16,6 @@ router.get('/', (req, res) => {
 }, (err)=>{
     console.log(err);
 } );
+// app.use('/sports', express.static(path.join(__dirname, '../../client/src/components/DisplayGames.jsx')));
 
 export default router;
