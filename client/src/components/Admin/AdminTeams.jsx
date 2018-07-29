d72e6103780024ef333aa26f8f5753199a86fdfb
percussion1313 <peterjacobgrant@gmail.com>
2018-07-28 19:40:34 -0500

    Created skeleton for team a and b page after games

---
+++ client/src/components/Admin/AdminTeams.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+//This area will show information on Team A and B, before the final bet
+class AdminTeams extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+
+        };
+    }
+
+    render() {
+        return ('')
+    }
+}
+
+export default AdminTeams;
