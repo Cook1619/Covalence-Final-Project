471c0467696e9045db8441904c0882e322177690
Cook1619 <cook6348@gmail.com>
2018-08-01 11:55:49 -0500

    Added location to the card

--- client/src/components/GameDetails.jsx
+++ client/src/components/GameDetails.jsx
@@ -13,7 +13,7 @@ class GameDetails extends Component {
             <div className="col-md-6 position-static">
                 <div className="card post-body p-2 m-4 bg-light rounded-3">
                     <div className="card-header bg-dark text-white rounded-0">
-                        <h5>Matchup Preview</h5>
+                        <h5>{games.location}</h5>
                     </div>
                     <div className="card-body">
                         <div className="card-text">
