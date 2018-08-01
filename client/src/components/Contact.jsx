5e97ac7d7ded66adfec1298313039eccd43be2c4
percussion1313 <peterjacobgrant@gmail.com>
2018-08-01 16:27:06 -0500

    removed merge conflict error

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -42,24 +42,18 @@ class Contact extends Component {
             <div className="container">
                 <form className="p-7" onSubmit={(e) => this.handleSubmit(e)}>
                     <div className="form-group">
-<<<<<<< HEAD
-<<<<<<< HEAD
                         <label htmlFor="name">Name</label>
                         <input placeholder="Name" onChange={(e) => this.handleName(e.target.value)} id="name" type="text" className="form-control rounded-0" required />
-=======
                         <label className="float-left" htmlFor="name">Name</label>
                         <input placeholder="Name" onChange={(e) => this.handleName(e.target.value)} name="name" type="text" className="form-control rounded-0" required />
->>>>>>> ad81901654d305abaf0f8a7395aa2131fc40453f
                     </div>
                     <div className="form-group">
                         <label htmlFor="email">Email</label>
                         <input placeholder="Email" onChange={(e) => this.handleEmail(e.target.value)} id="mail" type="email" className="form-control rounded-0" required />
-=======
                         <input htmlFor="name" placeholder="Name" onChange={(e) => this.handleName(e.target.value)} name="name" type="text" className="form-control rounded-0" required />
                     </div>
                     <div className="form-group">
                         <input htmlFor="email" placeholder="Email" onChange={(e) => this.handleEmail(e.target.value)} email="mail" type="email" className="form-control rounded-0" required />
->>>>>>> 0db9c020f1185eb1faf47503d7ea2cdbac05fbe9
                     </div>
                     <div className="form-group">
                         <textarea onChange={(e) => this.handleMessage(e.target.value)} cols="30" rows="10" className="form-control rounded-0"></textarea>
