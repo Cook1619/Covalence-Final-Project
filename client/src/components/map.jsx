c7f83a5ca911ddf4e65d6ca2973e9d5b03d8f4f1
Cook1619 <cook6348@gmail.com>
2018-08-02 08:43:15 -0500

    hiding api key now

--- client/src/components/map.jsx
+++ client/src/components/map.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import GoogleMaps from "simple-react-google-maps"
-import GOOGLE_SK from '../../../server/src/config/env/development'
+import { config } from '../../../server/src/config'
 
 const Map = (props) => {
 
     return (
         <React.Fragment>
             <GoogleMaps
-                apiKey={''}
+                apiKey={config.GOOGLE_SK}
                 style={{ height: "400px", width: "70%" }}
                 zoom={15}
                 center={{ lat: 44.5013, lng: -88.0622 }}
