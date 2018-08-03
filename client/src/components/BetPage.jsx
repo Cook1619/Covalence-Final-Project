9dd8016882c253e0a0a51fa25c23fbd6924afa51
Cook1619 <cook6348@gmail.com>
2018-08-03 10:21:53 -0500

    built out the BetPage component

--- client/src/components/BetPage.jsx
+++ client/src/components/BetPage.jsx
@@ -1,16 +1,43 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import BetDisplay from './BetDisplay';
+
+let apiKey = 'ZTkxZmFhYzAtYmFjMi00ZjdlLWFmZGUtODUzZWY3Oktpbm84MTY3MjE0MSE=';
+let sportsURL = 'https://api.mysportsfeeds.com/v1.0/pull/mlb/2018-regular/scoreboard.json?fordate=20180625';
 
 class BetPage extends Component {
     constructor(props) {
         super(props);
-
         this.state = {
-
+            games: []
         };
     }
 
+
+    componentWillMount() {
+        fetch(sportsURL, {
+            headers: new Headers({
+                'Content-Type': 'application/json',
+                'Authorization': `Basic ${apiKey}`
+            })
+        }).then(res => res.json())
+            .then(response => this.setState({
+                games: response.scoreboard.gameScore
+            }));
+    }
+
     render() {
-        return ('')
+        let pastGames = this.state.games.map((game, index) => {
+            return <BetDisplay key={index} game={game} />
+        })
+        return (
+            <Fragment>
+                <div className="container-fluid mt-5">
+                    <div className="row">
+                        {pastGames}
+                    </div>
+                </div>
+            </Fragment>
+        )
     }
 }
 
--- server/src/routes/games.js
+++ server/src/routes/games.js
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import axios from 'axios';
+import { config } from '../config'
 import Table from '../table';
 
 
@@ -15,22 +16,19 @@ router.get('/', async (req, res) => {
             method: 'get',
             url: `https://api.mysportsfeeds.com/v1.0/pull/mlb/2018-regular/scoreboard.json?fordate=20180801`,
             auth: {
-                username: process.env.SPORTS_SK,
-                password: process.env.SPORTS_PW
+                username: config.SPORTS_SK,
+                password: config.SPORTS_PW
             }
         });
-<<<<<<< HEAD
 
         console.log(testData.data);
-=======
         
-         let scoreData = testData.data.scoreboard.gameScore.game.awayScore;
+        let scoreData = testData.data.scoreboard.gameScore;
         console.log(scoreData);
 
         // scores.insert(scoreData);
 
         // scores.insert(scoreboard);
->>>>>>> 56cf426de815dd7b1fde6e27006180e338df47d8
 
         res.status(200).send('Ok');
     } catch (err) {
