b8162ccc40282b1650f67a3e25034afc8095fbfd
Cook1619 <cook6348@gmail.com>
2018-08-06 16:04:10 -0500

    Betpage showing a single game now

--- client/src/components/Admin/BetPage.jsx
+++ client/src/components/Admin/BetPage.jsx
@@ -12,7 +12,7 @@ class BetPage extends Component {
     async componentDidMount() {
         let id = this.props.match.params.id;
         try {
-            let res = await fetch('https://strikezone.herokuapp.com/api/futuregames/2');
+            let res = await fetch('/api/futuregames/' + id);
             let data = await res.json();
             console.log(data);
             this.setState({
@@ -24,21 +24,18 @@ class BetPage extends Component {
     }
 
     render() {
-      console.log(this.state.game)
+
         return (
             <Fragment>
                 <div className="col-md-12 position-static">
                     <div className="card post-body p-2 m-4 bg-light rounded-0 shadow-lg">
                         <div className="card-header bg-dark text-white rounded-0">
-                            <h5 className="text-center mt-3">Matchup Preview</h5>
+                            <h5 className="text-center mt-3">{this.state.game.game_location}</h5>
                         </div>
                         <div className="card-body">
-                            <div className="card-text">
-                                {`${game.awayTeam_City}   ${game.awayTeam_Name} VS ${game.homeTeam_City}   ${game.homeTeam_Name}`}
+                            <div className="card-text text-center">
+                                {`${this.state.game.awayTeam_City}   ${this.state.game.awayTeam_Name} VS ${this.state.game.homeTeam_City}   ${this.state.game.homeTeam_Name}`}
                             </div>
-                            <div></div>
-                            <div>At</div>
-                            <div>{`${game.game_location}`}</div>
                         </div>
                     </div>
                 </div>
