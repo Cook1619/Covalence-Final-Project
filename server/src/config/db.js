9fd7dd930d90752c61f5b10445d99f244a133922
percussion1313 <peterjacobgrant@gmail.com>
2018-08-07 11:19:51 -0500

    updated db

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -2,19 +2,11 @@ import mysql from 'mysql';
 
 let pool = mysql.createPool({
     connectionLimit: 10,
-    host: 'localhost',
-    user: 'root',
-    password: 'password',
-    database: 'strikezone'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME
 });
-
-// let pool = mysql.createPool({
-//     connectionLimit: 10,
-//     host: process.env.DB_HOST,
-//     user: process.env.DB_USER,
-//     password: process.env.DB_PASS,
-//     database: process.env.DB_NAME
-// });
 
 async function executeQuery(sql, args = []) {
     let connection = await getConnection();
