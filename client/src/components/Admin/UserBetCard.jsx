8d754de652c33dc99742572c4ec087314a0a9845
Luke Przekwas <przekwas@gmail.com>
2018-08-08 23:27:30 -0500

    adjusting prop name to display teamName

--- client/src/components/Admin/UserBetCard.jsx
+++ client/src/components/Admin/UserBetCard.jsx
@@ -6,9 +6,9 @@ export default class UserBetCard extends Component {
         return (
             <div className="card bg-dark mt-4 col-4 border-dark p-3 m-1 shadow">
                 <div className="card-header text-center bg-light"><u>Amount Bet:</u>
-                    <div className="card-text text-center">${this.props.bet.amount}</div>
+                    <div className="card-text text-center"><b>${this.props.bet.amount}</b></div>
                     <div className="card-header text-center bg-light"><u>On Team:</u>
-                        <div className="card-text text-center">{this.props.bet.teamid}</div>
+                        <div className="card-text text-center"><i>{this.props.bet.teamName}</i></div>
                         <Link className="btn btn-sm btn-dark float-bottom mt-2" to="/futuregames">Bet Again!</Link>
                     </div>
                 </div>
