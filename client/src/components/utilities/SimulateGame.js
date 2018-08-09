b291411b4a24cddab1ccdf7588d80f5c49fbe89f
Luke Przekwas <przekwas@gmail.com>
2018-08-08 21:47:38 -0500

    starting simulate game button

---
+++ client/src/components/utilities/SimulateGame.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default class SimulateGame extends Component {
+    render() {
+        return(
+            <div>Testing Component</div>
+        );
+    }
+}
