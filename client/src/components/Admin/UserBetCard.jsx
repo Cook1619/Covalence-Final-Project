58585784f803df46f0d0ac3fe9ccbd881ac60737
Luke Przekwas <przekwas@gmail.com>
2018-08-08 23:44:42 -0500

    logging switch statement correctly on is_winning_bet

--- client/src/components/Admin/UserBetCard.jsx
+++ client/src/components/Admin/UserBetCard.jsx
@@ -2,7 +2,20 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
 export default class UserBetCard extends Component {
+
+    renderWinOrLose() {
+        switch (this.props.bet.is_winning_bet) {
+            case 1:
+                return console.log('winner');
+            case 2:
+                return console.log('loser');
+            default:
+                return console.log('undetermined');
+        }
+    }
+
     render() {
+        this.renderWinOrLose();
         return (
             <div className="card bg-dark mt-4 col-3 border-dark p-3 m-2 shadow">
                 <div className="card-header text-center bg-light"><u>Amount Bet:</u>
