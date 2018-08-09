2449f59ab6585ba1de9dec62addb664a0141e42a
Luke Przekwas <przekwas@gmail.com>
2018-08-08 22:29:14 -0500

    starting put route to update winning bet col

--- server/src/routes/fakeGameResult.js
+++ server/src/routes/fakeGameResult.js
@@ -4,9 +4,24 @@ import Table from '../table';
 let router = Router();
 let betTable = new Table('bets');
 
-router.get('/', async (req, res) => {
+router.get('/:id', async (req, res) => {
+    let id = req.params.id;
     try {
-        res.status(200).send('Basic route ok!');
+        let getBetsTest = await betTable.getOne(id);
+        console.log(getBetsTest);
+        res.status(200).send(getBetsTest);
+    } catch (err) {
+        console.log(`Error getting bet details! ${error}`);
+        res.status(500).send(`Error getting bet details! ${error}`);
+    }
+});
+
+router.put('/:id', async (req, res) => {
+    let id = req.params.id;
+    try {
+        let getBetsTest = await betTable.getAll();
+        console.log(getBetsTest);
+        res.status(200).send(getBetsTest);
     } catch (err) {
         console.log(`Error getting bet details! ${error}`);
         res.status(500).send(`Error getting bet details! ${error}`);
