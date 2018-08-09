9e028120d5aed45af501a51adab5a40d54ac2977
Luke Przekwas <przekwas@gmail.com>
2018-08-08 22:02:09 -0500

    adjusting file type and basic wiring

--- client/src/components/utilities/SimulateGame.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-
-export default class SimulateGame extends Component {
-    render() {
-        return(
-            <div>Testing Component</div>
-        );
-    }
-}
---
+++ client/src/components/utilities/SimulateGame.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+export default class SimulateGame extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    simulateGame() {
+        let result = await fetch('/api/fake_it/');
+        console.log(result);
+    }   
+
+    render() {
+        return(
+            <div>
+                <button className='btn btn-success' onClick={() => this.simulateGame()}>Simulate Game</button>
+            </div>
+        );
+    }
+}
