0db5ae565ddd96e513b3763300e3067dcc8df3c2
Cook1619 <cook6348@gmail.com>
2018-08-09 06:55:42 -0500

    alignment is better with the betPage, still not perfect

--- client/src/components/Admin/BetPage.jsx
+++ client/src/components/Admin/BetPage.jsx
@@ -26,14 +26,14 @@ class BetPage extends Component {
         console.log(this.state.game[0])
         return (
             <Fragment>
-                <div className="col-md-12 margin-top large-margin-bottom border-2">
-                    <div className="card post-body p-2 m-4 bg-light rounded-0 shadow-lg">
-                        <div className="card-header bg-dark text-white rounded-0 row">
+                <div className="col-lg-12 margin-top large-margin-bottom border-2">
+                    <div className="card post-body p-4 m-4 bg-light rounded-0 shadow-lg">
+                        <div className="card-header bg-dark text-white rounded row">
                             <div className="col-md-4 pull-right">
                                 <img src={this.state.game.awayTeam} alt="" className="logo-left d-flex mx-auto" />
                             </div>
                             <div className="col-md-4 ">
-                                <h5 className="text-center mt-3">{`${this.state.game.game_location}`} </h5>
+                                <h5 className="text-center mt-3">{`${this.state.game.game_location}`}</h5>
                                 <h5 className="mt-3 text-center"><Moment format="MM/DD/YYYY">{`${this.state.game.game_date}`}</Moment></h5>
                                 <h5 className="mt-3 text-center">{this.state.game.game_time}</h5>
                             </div>
@@ -45,7 +45,7 @@ class BetPage extends Component {
                         <div className="card-body">
                             <div className="card-text col">
                                 <div>
-                                    <h1 className="float-left mt-3 col-md-6 text-center">
+                                    <h1 className="float-left mt-3 col-md-4 text-center">
                                         {`${this.state.game.awayTeam_City}   ${this.state.game.awayTeam_Name}`}
                                         <div className="card-body">
                                             <div>
@@ -67,7 +67,7 @@ class BetPage extends Component {
                                     </h1>
                                 </div>
                                 <div>
-                                    <h1 className="float-left mt-3 col-md-6 text-center">
+                                    <h1 className="float-right mt-3 col-md-4 text-center">
                                         {`${this.state.game.homeTeam_City}   ${this.state.game.homeTeam_Name}`}
                                         <div className="card-body">
                                             <div>
